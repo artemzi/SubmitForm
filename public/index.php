29e2b7d1ff8dc18ff7ea5dfb9287a612cbbbea29
Artem Zinoviev <arzinoviev@gmail.com>
2014-11-10 13:48:51 +0300

    change stryles

--- public/index.php
+++ public/index.php
@@ -13,34 +13,50 @@
             body {
                 background-image: url("img/marble.jpg");
             }
+            .jumbotron {
+                background-image: url("img/marble.jpg");
+            }
+            .img-responsive {
+                margin: 0 auto;
+            }
         </style>
 	</head>
 	<body>
-
+        <div class="jumbotron">
+            <a href="#" style="margin-top: 15px;">
+                <img class="img-responsive" src="img/logo-white.png">
+            </a>
+        </div>
         <div class="container">
             <div class="row-fluid">
-                <a href="#" class="pull-right" style="margin-top: 15px;">
-                    <img src="img/logo-white.png">
-                </a>
-            </div>
-            <div class="row-fluid">
+
                 <form role="form">
-                    <div class="form-group">
-                        <!--                    <label for="InputText">Some text</label>-->
-                        <input type="test" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    <div class="col-xs-32 col-sm-2 col-md-2 col-lg-2">
+
                     </div>
-                    <div class="form-group">
-                        <!--                    <label for="InputText">Some text</label>-->
-                        <input type="test" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                        <div class="form-group">
+                            <!--                    <label for="InputText">Some text</label>-->
+                            <input type="test" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <!--                    <label for="InputText">Some text</label>-->
+                            <input type="test" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <br/>
+                        <div class="form-group">
+                            <label for="exampleInputFile">File input</label>
+                            <input type="file" id="exampleInputFile">
+                            <p class="help-block">Example block-level help text here.</p>
+                        </div>
+                        
+                            <button type="submit" class="btn btn-success" style="width: 100%;">Submit</button>
+                            <button type="submit" class="btn btn-danger" style="width: 100%;">Clear</button>
+
                     </div>
-                    <br/>
-                    <div class="form-group">
-                        <label for="exampleInputFile">File input</label>
-                        <input type="file" id="exampleInputFile">
-                        <p class="help-block">Example block-level help text here.</p>
+                    <div class="col-xs-32 col-sm-2 col-md-2 col-lg-2">
+
                     </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                    <button type="submit" class="btn btn-default pull-right">Clear</button>
                 </form>
             </div>
         </div>
